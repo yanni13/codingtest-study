30dad377ac9f7886cd9c07f4a3ad86eb82c429c0
yanni <122153297+yanni13@users.noreply.github.com>
2025-01-12 00:15:18 -0800

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    temp = s.split(' ')
+    
+    for i in temp:
+        for j in range(len(i)):
+            if j % 2 == 0:
+                answer += i[j].upper()
+            else:
+                answer += i[j].lower()
+
+        answer += ' '
+    return answer[0:-1]
