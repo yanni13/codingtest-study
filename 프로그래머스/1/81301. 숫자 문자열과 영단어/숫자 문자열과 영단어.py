f2f80d081dca20510deb6dab31e48b374509f612
yanni <122153297+yanni13@users.noreply.github.com>
2025-03-23 23:23:03 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,7 @@
+def solution(s):
+    answer = s
+    temp = {'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9' }
+
+    for key, value in temp.items():
+        answer = answer.replace(key, value)
+    return eval(answer)
