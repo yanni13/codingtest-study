cdbdf68d0ce86b7925d78c81d1fcafdf948d060f
yanni <122153297+yanni13@users.noreply.github.com>
2025-03-25 20:43:45 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,7 +1,9 @@
 def solution(s):
     answer = s
-    temp = {'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9' }
-
-    for key, value in temp.items():
-        answer = answer.replace(key, value)
-    return eval(answer)
+    
+    data = {'0':'zero', '1':'one', '2':'two', '3':'three', '4':'four', '5':'five', '6':'six', '7':'seven', '8':'eight', '9':'nine'}
+    
+    for key, value in data.items():
+        # replace는 '문자열'을 변경
+        answer = answer.replace(value, key)
+    return int(answer)
