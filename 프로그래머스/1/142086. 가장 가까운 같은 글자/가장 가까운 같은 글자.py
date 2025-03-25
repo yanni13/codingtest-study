42d7114e393de67c6113b842a94889e94b21cd6a
yanni <122153297+yanni13@users.noreply.github.com>
2025-03-25 23:39:15 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.09 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,18 +1,8 @@
 def solution(s):
     answer = []
-    temp = []
-    last_index = {}
+    temp = [s]
     #얼마나 반복할 것인가? -> s의 글자만큼
-
     for i in range(len(s)):
-        if s[i] not in temp:
-            temp.append(s[i])
-            answer.append(-1)
-            
-        #s[i]이 이미 나온경우
-        else:
-            answer.append(i - last_index[s[i]])
-        
-        last_index[s[i]] = i
-
+        if temp[i+1] not in answer:
+            print(1)
     return answer
