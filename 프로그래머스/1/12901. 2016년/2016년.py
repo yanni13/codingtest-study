73ec57c101e3abc484d6e8c45c6666429738acfc
yanni <122153297+yanni13@users.noreply.github.com>
2025-04-14 22:54:53 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,13 +1,11 @@
 def solution(a, b):
     answer = ''
-    weekday = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
-    month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+    weekend = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     day = b
-    
     for i in range(a):
         day += month[i]
-
     day %= 7
-    answer = weekday[day]
-    return answer
+    
+    return weekend[day]
