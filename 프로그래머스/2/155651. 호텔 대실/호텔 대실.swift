8def68520831fe1d388180ea9e781fd8099ece12
yanni <122153297+yanni13@users.noreply.github.com>
2025-06-17 16:50:37 +0900

    [level 2] Title: 호텔 대실, Time: 158.50 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.swift
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.swift
@@ -1,22 +1,28 @@
 import Foundation
 
-func solution(_ book_time:[[String]]) -> Int {
-    var timeline = Array(repeating: 0, count: 24 * 60 + 10 + 1) // 최대 시간 + 청소시간
-
-    for times in book_time {
-        let start = timeStringToMinutes(times[0])
-        let end = timeStringToMinutes(times[1]) + 10
-
-        for i in start..<end {
-            timeline[i] += 1
-        }
-    }
-
-    return timeline.max()!
+func timeToString(_ time: String) -> Int {
+    let parts = time.split(separator: ":").map { Int($0)! } 
+    return parts[0] * 60 + parts[1]
 }
 
+func solution(_ book_time:[[String]]) -> Int {
+    let times = book_time.map {time -> (Int, Int) in 
+        let startTime = timeToString(time[0])
+        let endTime = timeToString(time[1]) + 10
+        return (startTime, endTime) }
+    
+    let sortedTime = times.sorted {$0.0 < $1.0}
+    
+    var rooms: [Int] = []
 
-func timeStringToMinutes(_ time: String) -> Int {
-    let parts = time.split(separator: ":").map { Int($0)! }
-    return parts[0] * 60 + parts[1]
+    for (start, end) in sortedTime {
+        if let index = rooms.firstIndex(where: {$0<=start}) {
+            rooms[index] = end
+        } else {
+            rooms.append(end)
+        }
+        rooms.sort()
+
+    }
+    return rooms.count
 }
